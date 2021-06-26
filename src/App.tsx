b22072c02a40a84867a438909eb58dc3c92e8481
Rubens Monteiro <rubens.monteiro@multiedro.com.br>
2021-06-26 12:42:50 -0300

    aula 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { Home } from './pages/Home'
 import { NewRoom } from './pages/NewRoom'
+import { Room } from './pages/Room'
+import { AdminRoom } from './pages/AdminRoom'
 
 import { AuthContextProvider } from './context/AuthContext'
 
@@ -10,8 +12,12 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path='/' exact component={Home} />
-        <Route path='/rooms/new' component={NewRoom} />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/rooms/new' component={NewRoom} />
+          <Route path='/rooms/:id' component={Room} />
+          <Route path='/admin/rooms/:id' component={AdminRoom}/>
+        </Switch>
       </AuthContextProvider>
     </BrowserRouter>
   );
